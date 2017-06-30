bc1ac70ebff1f8dd9bea84e8f903787ee20a77c9
Ian Cottam <ian.cottam@bbc.co.uk>
2017-06-30 11:26:39 +0100

    [IC] - project shell

---
+++ js/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Main extends React.Component {
+  constructor(props) {
+       super(props);
+  }
+
+    render() {
+        return (
+            <p>Test</p>
+            );
+    }
+}
